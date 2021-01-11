10e018182d4415549b138b19acfcd2c5ac8dd78e
yuuki <yuki.tsubo@gmail.com>
2021-01-11 21:20:23 +0900

    remove some default comments

--- cmd/connect.go
+++ cmd/connect.go
@@ -24,13 +24,7 @@ import (
 // connectCmd represents the connect command
 var connectCmd = &cobra.Command{
 	Use:   "connect",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "connect connects to a port where 'serve' listens",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("connect called")
 	},
@@ -38,14 +32,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(connectCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// connectCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// connectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

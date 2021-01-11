66ad436783e6483568be00b1e581d0cdd6e0e09c
yuuki <yuki.tsubo@gmail.com>
2021-01-11 22:24:17 +0900

    serve: add a simple echo server

--- cmd/serve.go
+++ cmd/serve.go
@@ -16,7 +16,14 @@ limitations under the License.
 package cmd
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -25,11 +32,63 @@ import (
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "serve accepts connections",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("serve called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		lport := cmd.Flags().Uint16P("port", "p", 9100, "listening port")
+		return serve(*lport)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(serveCmd)
+}
+
+func serve(lport uint16) error {
+	addr := fmt.Sprintf(":%d", lport)
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("listen %q error: %s", addr, err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			if ne, ok := err.(net.Error); ok {
+				if ne.Temporary() {
+					log.Printf("temporary error when listening for TCP addr %q: %s", ln.Addr(), err)
+					time.Sleep(time.Second)
+					continue
+				}
+				if strings.Contains(err.Error(), "use of closed network connection") {
+					break
+				}
+				log.Fatalf("unrecoverable error when accepting TCP connections: %s", err)
+			}
+			log.Fatalf("unexpected error when accepting TCP Graphite connections: %s", err)
+		}
+		go func() {
+			if err := echoStream(conn); err != nil {
+				log.Println(err)
+				if err := conn.Close(); err != nil {
+					log.Println("could not close: %s", err)
+				}
+			}
+		}()
+	}
+
+	return nil
+}
+
+func echoStream(conn net.Conn) error {
+	// if err := conn.SetReadDeadline(time.Now().Add(3 * time.Second)); err != nil {
+	// 	log.Printf("could not set read deadline: %s\n", err)
+	// }
+	r := bufio.NewReader(conn)
+	msg, err := ioutil.ReadAll(r)
+	if err != nil {
+		return errors.Errorf("could not read: %s\n", err)
+	}
+	if _, err := conn.Write(msg); err != nil {
+		return errors.Errorf("could write %q", msg)
+	}
+	return nil
 }

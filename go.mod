bd2b20e2a26dfb63a9c6bf3a59561e0c7c37f0f9
yuuki <yuki.tsubo@gmail.com>
2021-02-15 17:11:21 +0900

    add utility to set rlimit no file

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/spf13/cobra v1.1.1
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
+	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7
 )
---
+++ limit/limit.go
@@ -0,0 +1,27 @@
+package limit
+
+import (
+	"syscall"
+
+	"golang.org/x/xerrors"
+)
+
+// SetRLimitNoFile avoids too many open files error.
+func SetRLimitNoFile() error {
+	var rLimit syscall.Rlimit
+
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	if err != nil {
+		return xerrors.Errorf("could not get rlimit: %w", err)
+	}
+
+	if rLimit.Cur < rLimit.Max {
+		rLimit.Cur = rLimit.Max
+		err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+		if err != nil {
+			return xerrors.Errorf("could not set rlimit: %w", err)
+		}
+	}
+
+	return nil
+}
